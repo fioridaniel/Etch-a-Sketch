6dd61babadc08666fb781172ed6af94a91360b2f
fiori_daniel <daniel.fiori.souza@uel.br>
2026-07-12 12:12:42 -0300

    feat: add simple hovering effect

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-const ROWS = 8;
-const COLS = 8;
+const ROWS = 16;
+const COLS = 16;
 
-const grid = document.querySelector(".grid");
-
-for(let r = 0; r < ROWS; r++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    
-    for(let c = 0; c < COLS; c++) {
-        const cell = document.createElement("div");
-        cell.className = "row-cell";  
-        
-        row.appendChild(cell);
-    }    
-    grid.appendChild(row);
+function addHoverEffect(cell) {
+    cell.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "black";
+    });
 }
+
+function createGrid() {
+    const grid = document.querySelector(".grid");
+    
+    for(let r = 0; r < ROWS; r++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        
+        for(let c = 0; c < COLS; c++) {
+            const cell = document.createElement("div");
+            cell.className = "row-cell";      
+            row.appendChild(cell);
+
+            addHoverEffect(cell);
+        }    
+    
+        grid.appendChild(row);
+    }
+}
+
+createGrid();
